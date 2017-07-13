f515cdbdcee7d216890d99e464878a1381477e1a
Brad Jasper <bjasper@gmail.com>
2017-07-13 11:51:21 -0700

    Bump version

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 var config = {};
-config.version = "1.7.6";
+config.version = "1.8.0";
 config.host = "localhost:8918";
 config.wsendpoint = "ws://" + config.host + "/" + BrowserDetect.browser;
 config.blockURL = "http://localhost:8919/block/";
